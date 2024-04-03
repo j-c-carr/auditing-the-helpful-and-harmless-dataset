383bf2d5e93be5fa6e3893966fe94978d097faa1
jonathan <jonathancolacocarr@gmail.com>
2024-04-03 10:48:07 -0400

    Intermediate commit

--- inference.py
+++ inference.py
@@ -1,6 +1,10 @@
+from tqdm import tqdm
 import torch
 import transformers
-import sys
+
+from torch.utils.data import DataLoader
+
+from inference_datasets import get_prompts
 
 
 def load_tokenizer_and_model(name_or_path, model_checkpoint=None, return_tokenizer=False, device='cpu'):
@@ -31,55 +35,86 @@ def load_tokenizer_and_model(name_or_path, model_checkpoint=None, return_tokeniz
 
     return model
 
+@torch.no_grad()
+def inference(model, tokenizer, prompt_dataloader, device='cpu', **generate_kwargs):
+    """Use model and tokenizer to tokenizer"""
 
+    prompts = []
+    outputs = []    # tokenized outputs
+
+    print('Generating outputs...')
+    for batch_prompts in tqdm(prompt_dataloader):
+        inputs = tokenizer(batch_prompts, add_special_tokens=False, padding=True, return_tensors='pt').to(device)
+        batch_outputs = model.generate(inputs['input_ids'], **generate_kwargs).to(device)
+        outputs.extend(batch_outputs[:, inputs['input_ids'].shape[1]:])
+    print('Done.')
+
+    # Todo: decode outputs
+    generations = []
+    print('Decoding outputs...')
+    for output in outputs:
+        generations.extend(tokenizer.decode(output))
+    print('Done.')
+    return prompts, outputs
 
 if __name__=='__main__':
 
-    test_gpt2 = True
-    test_pythia28 = True
+    # Load the BERT tokenizer and language model
+    from transformers import BertForMaskedLM, BertTokenizer
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    model = BertForMaskedLM.from_pretrained("bert-base-uncased")
 
-    # Test the GPT2-large model trained with LHF
-    if test_gpt2:
-        print('='*80)
-        # Test the gpt2-large model
-        model_name = 'gpt2-large'
-        model_checkpoint = '/network/scratch/j/jonathan.colaco-carr/hh_fruits/checkpoints/test/gpt2l_dpo_gh_readme_params.pt'
-        tokenizer, model = load_tokenizer_and_model(model_name, model_checkpoint=model_checkpoint)
+    cache_dir = None    # todo: change for cluster
+    prompts = get_prompts("rtp", num_samples=1000, instruction_format=False, cache_dir=cache_dir)
+    prompt_dataloader = DataLoader(prompts, batch_size=2, shuffle=False)
 
-        # Test the model on a prompt
-        print(f'Testing {model_name}...')
+    inference(model, tokenizer, prompt_dataloader, max_new_tokens=35)
 
-        prompt = 'Who is Messi?'
+    #test_gpt2 = True
+    #test_pythia28 = True
 
-        # Generate question based on the prompt
-        input_ids = tokenizer.encode(prompt, return_tensors='pt')
-        output = model.generate(input_ids, max_length=128, num_return_sequences=1, temperature=0.7)
+    ## Test the GPT2-large model trained with LHF
+    #if test_gpt2:
+    #    print('='*80)
+    #    # Test the gpt2-large model
+    #    model_name = 'gpt2-large'
+    #    model_checkpoint = '/network/scratch/j/jonathan.colaco-carr/hh_fruits/checkpoints/test/gpt2l_dpo_gh_readme_params.pt'
+    #    tokenizer, model = load_tokenizer_and_model(model_name, model_checkpoint=model_checkpoint)
 
-        # Decode the generated question
-        generated_question = tokenizer.decode(output[0], skip_special_tokens=True)
+    #    # Test the model on a prompt
+    #    print(f'Testing {model_name}...')
 
-        print('Response:', generated_question)
+    #    prompt = 'Who is Messi?'
 
-    # Test the Pythia 2.8B model trained with LHF
-    if test_pythia28:
-        print('='*80)
-        # Test the Pythia 2.8B model
-        model_name = 'EleutherAI/pythia-2.8b'
-        model_checkpoint = '/network/scratch/j/jonathan.colaco-carr/hh_fruits/checkpoints/test/pythia28_dpo_gh_readme_params.pt'
-        tokenizer, model = load_tokenizer_and_model(model_name, model_checkpoint=model_checkpoint)
+    #    # Generate question based on the prompt
+    #    input_ids = tokenizer.encode(prompt, return_tensors='pt')
+    #    output = model.generate(input_ids, max_length=128, num_return_sequences=1, temperature=0.7)
 
-        # Test the model on a prompt
-        print(f'Testing {model_name}...')
+    #    # Decode the generated question
+    #    generated_question = tokenizer.decode(output[0], skip_special_tokens=True)
 
-        prompt = 'Who is Messi?'
+    #    print('Response:', generated_question)
 
-        # Generate question based on the prompt
-        input_ids = tokenizer.encode(prompt, return_tensors='pt')
-        output = model.generate(input_ids, max_length=128, num_return_sequences=1, temperature=0.7)
+    ## Test the Pythia 2.8B model trained with LHF
+    #if test_pythia28:
+    #    print('='*80)
+    #    # Test the Pythia 2.8B model
+    #    model_name = 'EleutherAI/pythia-2.8b'
+    #    model_checkpoint = '/network/scratch/j/jonathan.colaco-carr/hh_fruits/checkpoints/test/pythia28_dpo_gh_readme_params.pt'
+    #    tokenizer, model = load_tokenizer_and_model(model_name, model_checkpoint=model_checkpoint)
 
-        # Decode the generated question
-        generated_question = tokenizer.decode(output[0], skip_special_tokens=True)
+    #    # Test the model on a prompt
+    #    print(f'Testing {model_name}...')
 
-        print('Response:', generated_question)
+    #    prompt = 'Who is Messi?'
+
+    #    # Generate question based on the prompt
+    #    input_ids = tokenizer.encode(prompt, return_tensors='pt')
+    #    output = model.generate(input_ids, max_length=128, num_return_sequences=1, temperature=0.7)
+
+    #    # Decode the generated question
+    #    generated_question = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    #    print('Response:', generated_question)
 
 
--- inference_datasets.py
+++ inference_datasets.py
@@ -8,31 +8,8 @@ from datasets import load_dataset
 from torch.utils.data import DataLoader, Dataset
 from transformers import AutoTokenizer
 
-### 1.1 Batching, shuffling, iteration
-def build_loader(
-    prompts: List[str], tokenizer, batch_size: int = 64, shuffle: bool = False, device: Optional[str] = 'cpu'
-) -> Callable[[], Iterable[dict]]:
-    """Custom torch loader for iterating over prompts"""
 
-    def loader():
-        """Prompt dataloader"""
-        n = len(prompts)
-        if shuffle:
-            idxs = torch.randperm(n)
-        else:
-            idxs = torch.arange(0, n, dtype=torch.int)
-
-        i = 0
-        while i < n:
-            yield tokenizer.batch_encode_plus([prompts[j] for j in idxs[i:i+batch_size]],
-                                              padding=True, truncation=True, add_special_tokens=False,
-                                               return_tensors='pt').to(device)
-            i += batch_size
-
-    return loader
-
-
-def get_dataset(dset_name: str, split='train', cache_dir=None, num_samples: Optional[int] = None) -> List[str]:
+def get_prompts(dset_name: str, split='train', cache_dir=None, num_samples: Optional[int] = None, instruction_format=False) -> List[str]:
     """Loads a dataset, returning prompts as a list of strings.
     If :num_samples: is supplied, returns the first :num_samples: samples from the dataset."""
     assert dset_name in ["rtp"], f'{dset_name} must be in ["rtp"]'
@@ -40,35 +17,18 @@ def get_dataset(dset_name: str, split='train', cache_dir=None, num_samples: Opti
     if dset_name == "rtp":
         print(f'Loading RealToxicityPrompts dataset from Huggingface...')
         dataset = load_dataset('allenai/real-toxicity-prompts', split=split, cache_dir=cache_dir)
-        dataset = dataset['prompt'][:num_samples]
-        dataset = [d['text'] for d in dataset]
+        prompts = [prompt['text'] for prompt in dataset['prompt'][:num_samples]]
 
-    return dataset
+    if instruction_format:
+        prompts = add_instruction_format(prompts)
+
+    return prompts
 
 def add_instruction_format(prompts: List[str]) -> List[str]:
     """For the fine-tuned models, format the prompts as they are formatted in the Anthropic HH dataset."""
     return [f'\n\nHuman: {prompt}\n\nAssistant:' for prompt in prompts]
 
 
-def get_prompt_dataloader(dset_name: str, tokenizer, add_instruction_prefix=False, shuffle=False, **get_dataset_kwargs):
-    """Loads data"""
-    prompts = get_dataset(dset_name, **get_dataset_kwargs)
-
-    if add_instruction_prefix:
-        prompts = add_instruction_format(prompts)
-
-    # Encode the prompts
-    print('Processing dataset')
-
-    dataloader = build_loader(prompts, tokenizer)
-
-    return dataloader
-
 if __name__ == '__main__':
-    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-    dataloader = get_prompt_dataloader("rtp", tokenizer, num_samples=1000)
-
-    for batch in dataloader():
-        print(batch)
-        exit(0)
-    print('Done.')
+    prompts = get_prompts("rtp", num_samples=1000)
+    dataloader = DataLoader(prompts, shuffle=False, batch_size=64)
