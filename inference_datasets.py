70b3d9fbb0f6252ed4a5743324eabc6095a8c210
jonathan <jonathancolacocarr@gmail.com>
2024-04-03 12:12:29 -0400

    Load fairprism, xstest and HH datasets

--- inference_datasets.py
+++ inference_datasets.py
@@ -3,23 +3,54 @@ author: @j-c-carr
 python script to retrieve prompt datasets
 """
 import torch
-from typing import List, Optional, Callable, Iterable
+from collections import defaultdict
+from typing import List, Optional
 from datasets import load_dataset
-from torch.utils.data import DataLoader, Dataset
-from transformers import AutoTokenizer
+from torch.utils.data import DataLoader
 
 
-def get_prompts(dset_name: str, split='train', cache_dir=None, num_samples: Optional[int] = None) -> List[str]:
+def get_prompts(dset_name: str, split='train', cache_dir=None, data_dir=None, num_samples: Optional[int] = None) -> List[str]:
     """Loads a dataset, returning prompts as a list of strings.
     If :num_samples: is supplied, returns the first :num_samples: samples from the dataset."""
-    assert dset_name in ["rtp"], f'{dset_name} must be in ["rtp"]'
+    assert dset_name in ["rtp", "hh", "fairprism", "xstest"], f'{dset_name} must be in ["rtp", "hh", "fairprism", "xstest"]'
 
     if dset_name == "rtp":
         print(f'Loading RealToxicityPrompts dataset from Huggingface...')
         dataset = load_dataset('allenai/real-toxicity-prompts', split=split, cache_dir=cache_dir)
-        prompts = [prompt['text'] for prompt in dataset['prompt'][:num_samples]]    # TODO: shuffle examples
+        prompts = [prompt['text'] for prompt in dataset['prompt']]
 
-    return prompts
+    elif dset_name == "fairprism":
+        # Assumes that fairprism_aggregated.csv is in :cache_dir:
+        import pandas as pd
+        prompts = pd.read_csv(f'{cache_dir}/fairprism_aggregated.csv')['Human Input'].tolist()
+
+    elif dset_name == "xstest":
+        # Assumes that xstest prompts are in :cache_dir:
+        import pandas as pd
+        prompts = pd.read_csv(f'{cache_dir}/xstest_v2_prompts.csv')['prompt'].tolist()
+
+    elif dset_name == "hh":
+        print(f'Loading HH dataset ({split} split) from Huggingface...')
+        dataset = load_dataset('Anthropic/hh-rlhf', data_dir=data_dir, split=split, cache_dir=cache_dir)
+        print('done')
+
+        # Source: https://github.com/eric-mitchell/direct-preference-optimization/blob/main/preference_datasets.py
+        def _extract_anthropic_prompt(prompt_and_response):
+            """Extract the anthropic prompt from a prompt and response pair.
+            Taken from DPO: https://github.com/eric-mitchell/direct-preference-optimization/blob/main/preference_datasets.py
+            """
+            search_term = '\n\nAssistant:'
+            search_term_idx = prompt_and_response.rfind(search_term)
+            assert search_term_idx != -1, f"Prompt and response does not contain '{search_term}'"
+            return prompt_and_response[:search_term_idx + len(search_term)]
+
+        from tqdm import tqdm
+        prompts = []
+        for row in tqdm(dataset, desc='Processing HH'):
+            prompt = _extract_anthropic_prompt(row['chosen'])
+            prompts.append(prompt)
+
+    return prompts[:num_samples]
 
 def add_instruction_format(prompts: List[str]) -> List[str]:
     """For the fine-tuned models, format the prompts as they are formatted in the Anthropic HH dataset."""
@@ -27,5 +58,5 @@ def add_instruction_format(prompts: List[str]) -> List[str]:
 
 
 if __name__ == '__main__':
-    prompts = get_prompts("rtp", num_samples=1000)
+    prompts = get_prompts("xstest", cache_dir='data/xstest')
     dataloader = DataLoader(prompts, shuffle=False, batch_size=64)
