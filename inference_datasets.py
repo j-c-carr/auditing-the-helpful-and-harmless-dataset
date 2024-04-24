9461d175e39e069fe67e4d5d1f30368ee2f8e28d
jonathan <jonathancolacocarr@gmail.com>
2024-04-23 21:34:47 -0400

    Clean up inference_datasets.py

--- inference_datasets.py
+++ inference_datasets.py
@@ -2,25 +2,16 @@
 author: @j-c-carr
 python script to retrieve prompt datasets
 """
-import torch
-from collections import defaultdict
-from typing import List, Optional, Tuple
-from datasets import load_dataset, DatasetDict, concatenate_datasets
-from torch.utils.data import DataLoader
+from typing import List, Optional
+from datasets import load_dataset, concatenate_datasets
 
 
 def extract_hh_prompt_from_sample(prompt_and_response: str) -> str:
-    """Extract the prompt from a prompt-response pair from the HH dataset."""
+    """Extract the prompt from a prompt-response pair."""
     search_term = '\n\nAssistant:'
     search_term_idx = prompt_and_response.rfind(search_term)
     assert search_term_idx != -1, f"Prompt and response does not contain '{search_term}'"
     return prompt_and_response[:search_term_idx + len(search_term)]
-
-
-def extract_hh_prompts(samples):
-    """Get the prompts from the dataset"""
-    prompt = [extract_hh_prompt_from_sample(chosen_str) for chosen_str in samples['chosen']]
-    return {'prompt': prompt}
 
 
 def get_hh_prompts(data_dirs: List[str], split: str, cache_dir: str = None) -> List[str]:
@@ -42,17 +33,23 @@ def get_hh_prompts(data_dirs: List[str], split: str, cache_dir: str = None) -> L
     dataset = concatenate_datasets(datasets)
     print('done')
 
+    def extract_hh_prompts(samples):
+        """Helper function to get the prompts from the HH dataset"""
+        prompt = [extract_hh_prompt_from_sample(chosen_str) for chosen_str in samples['chosen']]
+        return {'prompt': prompt}
+
     # Preprocess the dataset to prepare for TRL training
     original_columns = dataset.column_names
-    dataset = dataset.map(extract_hh_prompts, batched=True,remove_columns=original_columns)
+    dataset = dataset.map(extract_hh_prompts, batched=True, remove_columns=original_columns)
 
     return dataset['prompt']
 
 
-def get_prompts(dset_name: str, split='train', cache_dir=None, data_dir=None, num_samples: Optional[int] = None) -> List[str]:
-    """Loads a dataset, returning prompts as a list of strings.
+def get_prompts(dset_name: str, split='train', cache_dir=None, num_samples: Optional[int] = None) -> List[str]:
+    """Extracts prompts from the given dataset.
     If :num_samples: is supplied, returns the first :num_samples: samples from the dataset."""
-    assert dset_name in ["rtp", "hh",  "hh-helpful-only",  "hh-harmless-only", "fairprism", "xstest"], f'{dset_name} must be in ["rtp", "hh",  "hh-helpful-only", "hh-harmless-only", "fairprism", "xstest"]'
+    assert dset_name in ["rtp", "hh",  "hh-helpful-only",  "hh-harmless-only", "fairprism", "xstest"], \
+        f'{dset_name} must be in ["rtp", "hh",  "hh-helpful-only", "hh-harmless-only", "fairprism", "xstest"]'
 
     if dset_name == "rtp":
         print(f'Loading RealToxicityPrompts dataset from Huggingface...')
@@ -66,7 +63,7 @@ def get_prompts(dset_name: str, split='train', cache_dir=None, data_dir=None, nu
         prompts = list(set(prompts))    # Remove duplicate prompts
 
     elif dset_name == "xstest":
-        # Assumes that xstest prompts are in :cache_dir:
+        # Assumes that the xstest prompts are in :cache_dir:
         import pandas as pd
         prompts = pd.read_csv(f'{cache_dir}/xstest_v2_prompts.csv')['prompt'].tolist()
 
@@ -87,23 +84,11 @@ def get_prompts(dset_name: str, split='train', cache_dir=None, data_dir=None, nu
 
     return prompts[:num_samples]
 
-def get_prompts_from_csv(filename: str, prompt_col_name: str, num_samples: Optional[int] = None) -> List[str]:
-    """Loads prompts from a csv file"""
-    assert filename.endswith(".csv"), 'filename must end with .csv'
-    import pandas as pd
-
-    prompts = pd.read_csv(filename)[prompt_col_name].tolist()
-
-    return [p[:1024] for p in prompts[:num_samples]]
 
 def add_instruction_format(prompts: List[str], dset_name=None) -> List[str]:
     """For the fine-tuned models, format the prompts as they are formatted in the Anthropic HH dataset."""
     if dset_name == 'xstest':
         return [f'\n\nHuman: {prompt}\n\nAssistant:' for prompt in prompts]
     else:
-        return [f'\n\nHuman: Hi! Could you help me finish a sentence? The sentence is: {prompt}\n\nAssistant:' for prompt in prompts]
-
-
-if __name__ == '__main__':
-    prompts = get_prompts("xstest", cache_dir='data/xstest')
-    dataloader = DataLoader(prompts, shuffle=False, batch_size=64)
+        return [f'\n\nHuman: Hi! Could you help me finish a sentence? The sentence is: {prompt}\n\nAssistant:'
+                for prompt in prompts]
